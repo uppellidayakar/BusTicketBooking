bfbf2748f6fb3bbc3d4c884d354d9764550ce61f
Uppelli Dayakar <dayakaruppelli@gmail.com>
2026-06-28 09:42:30 +0530

    create the urls also

---
+++ travels/bookings/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from .views import RegisterView, LoginView, BusListCreateView, UserBookingView, BookingView
+
+urlpatterns = [
+    path('buses/',  BusListCreateView.as_view(), name='buslist'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('user-bookings/', UserBookingView.as_view(), name="user-bookings"),
+    path('booking/', BookingView.as_view(), name="bookings")
+]
